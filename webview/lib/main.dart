c93e44a4a65536d20a826162248b4f77c61daa0c
mariam-2002saif <111151748+mariam-2002saif@users.noreply.github.com>
2023-10-20 21:46:04 +0300

    group's nouriah
    group's nouriah

---
+++ webview/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:connectivity/connectivity.dart';
+
+void main()
+{
+
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://flutter.dev'));
+  bool isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // تحقق من الاتصال بالانترنت
+    Connectivity().onConnectivityChanged.listen((connectivityResult) {
+      setState(() {
+        isConnected = connectivityResult == ConnectivityResult.mobile ||
+            connectivityResult == ConnectivityResult.wifi;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the platform implementation for the webview_flutter plugin.
+    WebViewPlatform.instance;
+    // تحقق من اتصال الإنترنت
+    if (!isConnected) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Flutter Simple Example')),
+        body: Center(child: Text('لا يوجد اتصال بالإنترنت')),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Flutter Simple Example')),
+        body: WebViewWidget(controller: controller),
+      );
+    }
+  }
+}
+
